53ec30e31495c8038ff7fc1c87b530de48adb6d6
Lambda <5417634+Lambda11@users.noreply.github.com>
2019-08-24 00:58:52 +0300

    Patch 85 untested

--- index.js
+++ index.js
@@ -17,7 +17,29 @@ module.exports = function FishDeleter(mod) {
 		myGameId = event.gameId;
 	})
 	
-	mod.hook('S_INVEN', mod.majorPatchVersion >= 80 ? 18 : 17, (event) => {
+	if(mod.majorPatchVersion >= 85)
+	{
+		mod.hook('S_ITEMLIST', 1, (event) => {
+			if (!enabled) return;
+			
+			for (var i = 0; i < event.items.length; i++)
+			{
+				if (event.items[i].id === filetId && event.items[i].amount > 9000)
+				{
+					mod.toServer('C_DEL_ITEM', 3, {
+						gameId: myGameId,
+						pocket: event.pocket,
+						slot: event.items[i].slot,
+						amount: 1000
+					});
+					break;
+				}
+			}
+		});
+	}
+	else
+	{
+		mod.hook('S_INVEN', 18, (event) => {
 		if (!enabled) return;
 		
 		for (var i = 0; i < event.items.length; i++)
@@ -33,4 +55,5 @@ module.exports = function FishDeleter(mod) {
 			}
 		}
 	});
+	}
 }
